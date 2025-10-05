b1e4d012a048592dbab960c8735c3b5ed474f87c
HriGrit <ayushsoam15@gmail.com>
2025-10-05 19:43:04 +0530

    feat: use dotenv for mongoose connection string

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import authRoutes from './routes/auth.js'
 import flightRoutes from './routes/flight.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 
-mongoose.connect('mongodb+srv://blogChefUser:y4dPJiTA5v1ZGu8T@cluster0.orjsiol.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }).then(() => console.log("MongoDB connected"));
@@ -18,4 +20,4 @@ app.use('/uploads', express.static('uploads'))
 app.use('/api/auth', authRoutes)        // user/ admin login or registration
 app.use('/api/flights', flightRoutes)   // adding/ updating/ deleting/ and fething flights
 
-app.listen(5001, ()=> console.log(' Server is running on port 5001..'))
+app.listen(5002, ()=> console.log(' Server is running on port 5002.'))
